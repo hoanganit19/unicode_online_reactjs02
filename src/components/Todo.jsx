8944947d5dd55eda2fe18f7b6322e8d1aa195b13
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-02-21 18:35:31 +0700

    useState - Todo App 02

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/style.css";
+import { useState } from "react";
+import { uid } from "../utils/uid";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [name, setName] = useState("");
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const todo = {
+      id: uid(),
+      name,
+      completed: false,
+    };
+    setTodoList([...todoList, todo]);
+    setName("");
+    toast.success("Add todo success");
+  };
   return (
     <div className="todos">
       <div className="w-50 mx-auto">
@@ -10,36 +30,51 @@ const Todo = () => {
           <span>Active</span>
           <span>Completed</span>
         </div>
-        <form action="">
+        <form action="" onSubmit={handleAdd}>
           <div className="input-group">
             <input
               type="text"
               className="form-control"
               placeholder="Please fill..."
+              onChange={handleChange}
+              value={name}
             />
             <button className="btn btn-success">Add</button>
           </div>
         </form>
         <div className="todo-list">
-          <div className="todo-item d-flex justify-content-between align-items-center my-3">
-            <input type="checkbox" />
-            <span>Tên công việc</span>
-            <div>
-              <button className="btn btn-danger btn-sm">Delete</button>
-            </div>
-          </div>
-          <div className="todo-item d-flex justify-content-between align-items-center my-3">
-            <input type="checkbox" />
-            <span>Tên công việc</span>
-            <div>
-              <button className="btn btn-danger btn-sm">Delete</button>
-            </div>
-          </div>
+          {todoList.length ? (
+            todoList.map(({ id, name, completed }) => (
+              <div
+                key={id}
+                className={`todo-item d-flex justify-content-between align-items-center my-3 ${
+                  completed ? "completed" : ""
+                }`}
+              >
+                <input type="checkbox" />
+                <span>{name}</span>
+                <div>
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-center py-2">No Todo</p>
+          )}
+
           <button className="btn btn-danger d-block mx-auto">Delete All</button>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
 
 export default Todo;
+
+/*
+State
+- todoList --> Lưu trữ danh sách công việc
+- name --> Lưu trữ tên công việc ở form
+- message --> Thông báo
+*/
